79df10d7d523ffe386a33db457dd6469e1f36574
Krzysztof <krzysztof.gebka2001@gmail.com>
2024-06-15 15:41:18 +0200

    Add button to adding the expenses userDetails section

--- frontend/src/UserDetails.js
+++ frontend/src/UserDetails.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import Modal from 'react-modal';
 
 const UserDetails = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const [user, setUser] = useState(null);
     const { eventId, userId } = useParams();
 
@@ -18,6 +20,21 @@ const UserDetails = () => {
 
     useEffect(handleGetEvent, [eventId, userId]);
 
+    const handleAddExpense = (event) => {
+        event.preventDefault();
+        axios.post(`http://localhost:8080/api/events/${eventId}/users/${userId}/expenses`, {
+            amount: event.target[0].value,
+            description: event.target[1].value
+        })
+            .then(res => {
+                handleGetEvent();
+                setModalIsOpen(false);
+            })
+            .catch(err => {
+                console.error('There was an error adding the expense!', err);
+            });
+    }
+
     return (
         <div>
             <h1>{user && user.name} Details</h1>
@@ -29,6 +46,21 @@ const UserDetails = () => {
                     </li>
                 ))}
             </ul>
+            <button onClick={() => setModalIsOpen(true)}>Add Expense</button>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                <h2>Add Expense</h2>
+                <form onSubmit={handleAddExpense}>
+                    <label>
+                        Amount:
+                        <input type="number" step={0.01} />
+                    </label>
+                    <label>
+                        Description:
+                        <input type="text" />
+                    </label>
+                    <button>Add</button>
+                </form>
+            </Modal>
         </div>
     );
 };
